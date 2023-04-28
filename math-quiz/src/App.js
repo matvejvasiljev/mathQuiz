a31607c797293a361e29f6a949940bd68ce3548f
matvejvasiljev <matvej.vasiljev.2010@icloud.com>
2023-04-28 16:49:23 +0300

    update

--- math-quiz/src/App.js
+++ math-quiz/src/App.js
@@ -8,24 +8,69 @@ class App extends React.Component {
         this.state = {
             questionBoxText: "Click the button to start the game!",
             questionAnswer: 0,
+            playerAnswer: "",
 
             score: 0,
+            maxTime: 60,
             time: 60,
+
+            difficulty: 1,
+
+            currentSign: "+",
         }
+        this.gameTimer = ""
+    }
+
+    gameStart(currentSign) {
+        this.setState(function (state) {
+            return{
+                score: 0,
+                time: state.maxTime,
+            }
+        })
+        clearInterval(this.gameTimer)
+        this.gameTimer = setInterval(() => {
+            this.setState(function (state) {
+                let questionBoxText = state.questionBoxText
+                if (state.time < 2) {
+                    clearInterval(this.gameTimer)
+                    questionBoxText = "Game over!"
+                }
+                return {
+                    time: state.time - 1,
+                    questionBoxText: questionBoxText,
+                }
+            })
+        }, 1000);
+        this.questionStart(currentSign)
     }
 
     questionStart(sign) {
         this.setState(function (state) {
-            let number1 = Math.floor(Math.random() * 100)
-            let number2 = Math.floor(Math.random() * 100)
-            let questionBoxText = number1 + sign + number2
+            let number1 = 0
+            let number2 = 0
+            if (sign === "/") {
+                number2 = Math.floor(Math.random() * 48 + 2)
+                number1 = number2 * Math.floor(Math.random() * 8 + 2)
+            }
+            else if (sign === "*") {
+                number1 = Math.floor(Math.random() * 18 + 2)
+                number2 = Math.floor(Math.random() * 8 + 2)
+            }
+            else {
+                number1 = Math.floor(Math.random() * 100)
+                number2 = Math.floor(Math.random() * 100)
+            }
+
+            let questionBoxText = number1 + sign + number2 + "=?"
             // eslint-disable-next-line
-            let questionAnswer = eval(questionBoxText)
+            let questionAnswer = eval(number1 + sign + number2)
             console.log(questionAnswer)
 
             return {
                 questionBoxText: questionBoxText,
                 questionAnswer: questionAnswer,
+                currentSign: sign,
             }
         })
     }
@@ -36,34 +81,40 @@ class App extends React.Component {
             console.log("answer submit");
             let score = state.score
 
-            if (0 === state.questionAnswer) {
+            if (parseInt(state.playerAnswer) === state.questionAnswer) {
                 console.log("question correct");
                 score += 1
             }
 
             return {
-                questionAnswer: 6,
+                playerAnswer: "",
+                questionAnswer: "",
                 score: score,
             }
+        }, function () {
+            this.questionStart(this.state.currentSign)
         })
     }
+
+
 
     render() {
         return (
             <div>
                 <form id="game" onSubmit={(e) => this.onSubmit(e)}>
                     <h1>Math Quiz</h1>
-                    <h3>{this.state.questionBoxText + "=?"}</h3>
+                    <h3>{this.state.questionBoxText}</h3>
                     <h2>{"Score: " + this.state.score}</h2>
                     <h2>{"time: " + this.state.time}</h2>
-                    <input type="text" placeholder="Your number" />
+                    <input value={this.state.playerAnswer} onChange={(e) => this.setState({ playerAnswer: e.target.value })} type="text" placeholder="Your number"></input>
                     {/* <button id="submitButton" onSubmit={(evt) => this.onSubmit(evt)}></button> */}
-                    <button id="submitButton" ></button>
+                    <button id="submitButton" className={this.state.buttonClass}></button>
+                    <button id="restartButton" type="button" onClick={() => this.gameStart(this.state.currentSign)}></button>
                     <div className="buttons">
-                        <button type="button" onClick={() => this.questionStart("+")}>+</button>
-                        <button type="button" onClick={() => this.questionStart("-")}>-</button>
-                        <button type="button" onClick={() => this.questionStart("/")}>/</button>
-                        <button type="button" onClick={() => this.questionStart("*")}>*</button>
+                        <button type="button" onClick={() => this.gameStart("+")}>+</button>
+                        <button type="button" onClick={() => this.gameStart("-")}>-</button>
+                        <button type="button" onClick={() => this.gameStart("/")}>/</button>
+                        <button type="button" onClick={() => this.gameStart("*")}>*</button>
                     </div>
                 </form>
             </div>

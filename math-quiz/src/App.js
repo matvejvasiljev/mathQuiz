46e56c4d72d23f4e749c8f9f5dd0e06e82c8f48a
matvejvasiljev <matvej.vasiljev.2010@icloud.com>
2023-05-05 14:37:54 +0300

    more difficulty options

--- math-quiz/src/App.js
+++ math-quiz/src/App.js
@@ -14,16 +14,17 @@ class App extends React.Component {
             maxTime: 60,
             time: 60,
 
-            difficulty: 1,
+            difficulty: 0,
+            numbers: "II",
 
-            currentSign: "+",
+            currentSign: "",
         }
         this.gameTimer = ""
     }
 
     gameStart(currentSign) {
         this.setState(function (state) {
-            return{
+            return {
                 score: 0,
                 time: state.maxTime,
             }
@@ -49,22 +50,52 @@ class App extends React.Component {
         this.setState(function (state) {
             let number1 = 0
             let number2 = 0
+            let number3 = 0
             if (sign === "/") {
-                number2 = Math.floor(Math.random() * 48 + 2)
-                number1 = number2 * Math.floor(Math.random() * 8 + 2)
+                number2 = Math.floor(Math.random() * 10 + 10)
+                number1 = number2 * Math.floor(Math.random() * 10 + 10)
+                if (state.difficulty === 0) {
+                    number2 = Math.floor(Math.random() * 8 + 2)
+                    number1 = number2 * Math.floor(Math.random() * 10 + 10)
+                }
             }
             else if (sign === "*") {
                 number1 = Math.floor(Math.random() * 18 + 2)
                 number2 = Math.floor(Math.random() * 8 + 2)
+                number3 = Math.floor(Math.random() * 8 + 2)
+
+                if (state.difficulty === 1) {
+                    number1 = Math.floor(Math.random() * 40 + 10)
+                }
+            }
+            else if (sign === "-") {
+                number1 = Math.floor(Math.random() * 119 + 1)
+                number2 = Math.floor(Math.random() * 99 + 1)
+                number3 = Math.floor(Math.random() * 99 + 1)
+
+                if (state.difficulty === 0) {
+                    number2 = Math.floor(Math.random() * number1 + 1)
+                    number3 = Math.floor(Math.random() * (number1 - number2) + 1)
+                }
             }
             else {
                 number1 = Math.floor(Math.random() * 100)
                 number2 = Math.floor(Math.random() * 100)
+                number3 = Math.floor(Math.random() * 100)
+                if (state.difficulty === 1) {
+                    number1 = Math.floor(Math.random() * 200 + 100)
+                    number2 = Math.floor(Math.random() * 90 + 10)
+                    number2 = Math.floor(Math.random() * 90 + 10)
+                }
             }
 
             let questionBoxText = number1 + sign + number2 + "=?"
             // eslint-disable-next-line
             let questionAnswer = eval(number1 + sign + number2)
+            if (state.numbers === "III" && sign !== "/") {
+            questionBoxText = number1 + sign + number2 + sign + number3 + "=?"
+            questionAnswer = eval(number1 + sign + number2 + sign + number3)
+            }
             console.log(questionAnswer)
 
             return {
@@ -107,14 +138,18 @@ class App extends React.Component {
                     <h2>{"Score: " + this.state.score}</h2>
                     <h2>{"time: " + this.state.time}</h2>
                     <input value={this.state.playerAnswer} onChange={(e) => this.setState({ playerAnswer: e.target.value })} type="text" placeholder="Your number"></input>
-                    {/* <button id="submitButton" onSubmit={(evt) => this.onSubmit(evt)}></button> */}
                     <button id="submitButton" className={this.state.buttonClass}></button>
                     <button id="restartButton" type="button" onClick={() => this.gameStart(this.state.currentSign)}></button>
                     <div className="buttons">
-                        <button type="button" onClick={() => this.gameStart("+")}>+</button>
-                        <button type="button" onClick={() => this.gameStart("-")}>-</button>
-                        <button type="button" onClick={() => this.gameStart("/")}>/</button>
-                        <button type="button" onClick={() => this.gameStart("*")}>*</button>
+                        <button className={this.state.currentSign === "+" ? "activeButton" : ""} type="button" onClick={() => this.gameStart("+")}>+</button>
+                        <button className={this.state.currentSign === "-" ? "activeButton" : ""} type="button" onClick={() => this.gameStart("-")}>-</button>
+                        <button className={this.state.currentSign === "/" ? "activeButton" : ""} type="button" onClick={() => this.gameStart("/")}>/</button>
+                        <button className={this.state.currentSign === "*" ? "activeButton" : ""} type="button" onClick={() => this.gameStart("*")}>*</button>
+                    </div>
+                    <div className="difficulties">
+                        <button className={this.state.difficulty === 0 ? "activeButton" : ""} type="button" onClick={() => this.setState({ difficulty: 0 })}>Easy</button>
+                        <button className={this.state.difficulty === 1 ? "activeButton" : ""} type="button" onClick={() => this.setState({ difficulty: 1 })}>Hard</button>
+                        <button onClick={() => this.setState(function (state) {return{numbers: state.numbers === "II" ? "III" : "II"}})} type="button">{this.state.numbers}</button>
                     </div>
                 </form>
             </div>
